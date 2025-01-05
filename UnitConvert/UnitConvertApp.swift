fb25b330a1e395b3ceefdc8c980ffd949bb59ebe
StanleyTack <stanley.tack@gmail.com>
2025-01-05 09:58:17 -0800

    Initial Commit

---
+++ UnitConvert/UnitConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConvertApp.swift
+//  UnitConvert
+//
+//  Created by Stanley Tack on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct UnitConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
